664fa40e53a734449db00b102465a84a6276d2c1
Omari Lyn <lynomari@gmail.com>
2024-10-21 09:04:28 -0400

    ArtSpace Project

---
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.artspace
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.artspace.data.DataSource
+import com.example.artspace.ui.theme.ArtSpaceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val navController = rememberNavController()
+            ArtSpaceTheme {
+                NavHost(navController = navController,
+                    startDestination = Screen.Home.route + "/{id}"
+                ){
+                    //Home page composable
+                    composable(
+                        Screen.Home.route + "/{id}", arguments = listOf(navArgument("id") {
+                            type = NavType.IntType
+                            defaultValue = 0
+                        })
+                    ){
+                        HomePage(navController = navController)
+                    }
+                    //Art page composable
+                    composable(
+                        Screen.Artist.route + "/{id}", arguments = listOf(navArgument("id") {
+                            type = NavType.IntType
+                        })
+                    ){
+                        ArtistPage(navController = navController)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ArtistPage(navController: NavController) {
+    Text(
+        text = "Artist Page"
+    )
+}
+
+@Composable
+fun HomePage(navController: NavController) {
+    //Displays current artwork
+    var current by remember {
+        mutableIntStateOf(
+            navController.currentBackStackEntry?.arguments?.getInt("id") ?: 0
+        )
+    }
+    val art = DataSource.arts[current]
+    Text(text = "Home Page ${stringResource(id = art.artistId)}")
+}
---
+++ app/src/main/java/com/example/artspace/data/DataSource.kt
@@ -0,0 +1,19 @@
+package com.example.artspace.data
+
+import com.example.artspace.R
+import com.example.artspace.model.Art
+
+object DataSource {
+    val arts = listOf(
+        Art(
+            R.string.art_1_artist_name,
+            R.string.art_1_artist_info,
+            R.string.art_1_title,
+            R.string.art_1_year,
+            R.string.art_1_description,
+            R.string.art_1_artist_bio,
+            R.drawable.artwork_image,
+            R.drawable.artist_image
+        )
+    )
+}
